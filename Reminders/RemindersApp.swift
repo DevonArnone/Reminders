6924360d5e9fef1c7aaef36cac031cc8f4dcbe08
Devon Arnone <87498263+DevonArnone@users.noreply.github.com>
2025-10-21 14:00:01 -0400

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Devon Arnone on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
